c4c8ee86db44c8ca6f1d0a364c0982ae1e988758
Aremu mahmud <74737123+aremumahmud@users.noreply.github.com>
2023-06-06 03:22:05 +0100

    Update about_pic.jsx

--- src/components/about_pic.jsx
+++ src/components/about_pic.jsx
@@ -3,10 +3,10 @@ import '../css/aboutPic.css'
 function AboutPic(){
     return (
         <div className="main">
-        <div className="card" id="c1"><img className='img' src="/imgs/kaboompics_colored-sequin-dresses-and-boots-lie-on-a-wooden-parquet-14943.jpg" alt="" /></div>
-        <div className="card" id="c2"><img  className='img' src="/imgs/kaboompics_colorful-sweaters-15376.jpg" alt="" /></div>
-        <div className="card" id="c3"><img  className='img' src="/imgs/kaboompics_sneakers-shoes-long-white-striped-socks-27740.jpg" alt="" /></div>
-        <div className="card" id="c4"><img  className='img' src="/imgs/kaboompics_white-shirt-glass-of-water-27507.jpg" alt="" /></div>
+        <div className="card34" id="c1"><img className='img' src="/imgs/kaboompics_colored-sequin-dresses-and-boots-lie-on-a-wooden-parquet-14943.jpg" alt="" /></div>
+        <div className="card34" id="c2"><img  className='img' src="/imgs/kaboompics_colorful-sweaters-15376.jpg" alt="" /></div>
+        <div className="card34" id="c3"><img  className='img' src="/imgs/kaboompics_sneakers-shoes-long-white-striped-socks-27740.jpg" alt="" /></div>
+        <div className="card34" id="c4"><img  className='img' src="/imgs/kaboompics_white-shirt-glass-of-water-27507.jpg" alt="" /></div>
     </div>
     )
 }
