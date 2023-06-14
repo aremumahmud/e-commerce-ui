0816daabee2670bc927372851c5691039a43bb80
mahmud aremu <aremumahmud2003@gmail.com>
2023-06-14 09:44:52 +0100

    added so,e changes

--- src/libs/uri.js
+++ src/libs/uri.js
@@ -1,10 +1,10 @@
 const pic = {
-    pic_uri: 'http://localhost:4000',
+    pic_uri: 'https://uploadimagefiles.aremzy.repl.co',
     upload_product_uri: 'https://e-commerce-api.aremzy.repl.co/v1/api/add_products',
     get_products_uri: 'https://e-commerce-api.aremzy.repl.co/v1/api/get_products',
     upload_locked_product_uri: 'https://e-commerce-api.aremzy.repl.co/v1/api/lock_inventory',
     upload_locked_product_uri_guest: 'https://e-commerce-api.aremzy.repl.co/v1/api/lock_inventory_guest',
-    fetch_orders_uri: 'https://e-commerce-api.aremzy.repl.co/v1/api/fetch_orders',
+    fetch_orders_uri: 'https://e-cozmmerce-api.aremzy.repl.co/v1/api/fetch_orders',
     get_categories: 'https://e-commerce-api.aremzy.repl.co/v1/api/get_category',
     send_otp: 'https://e-commerce-api.aremzy.repl.co/v1/api/send_otp',
     verify_otp: 'https://e-commerce-api.aremzy.repl.co/v1/api/verify_otp',
