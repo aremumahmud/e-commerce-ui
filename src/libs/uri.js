cfad6c7652f59ff943097d7655de352a356d4d8d
mahmud aremu <aremumahmud2003@gmail.com>
2023-06-17 13:56:28 +0100

    added some changes

--- src/libs/uri.js
+++ src/libs/uri.js
@@ -1,15 +1,15 @@
 const pic = {
     pic_uri: 'https://uploadimagefiles.aremzy.repl.co',
-    upload_product_uri: 'http://localhost:2000/v1/api/add_products',
-    get_products_uri: 'http://localhost:2000/v1/api/get_products',
-    upload_locked_product_uri: 'http://localhost:2000/v1/api/lock_inventory',
-    upload_locked_product_uri_guest: 'http://localhost:2000/v1/api/lock_inventory_guest',
-    fetch_orders_uri: 'http://localhost:2000/v1/api/fetch_orders',
-    get_categories: 'http://localhost:2000/v1/api/get_category',
-    send_otp: 'http://localhost:2000/v1/api/send_otp',
-    verify_otp: 'http://localhost:2000/v1/api/verify_otp',
-    change_password: 'http://localhost:2000/v1/api/change_password',
-    search_uri: 'http://localhost:2000/v1/api/search/search_products'
+    upload_product_uri: 'https://e-commerce-api.aremzy.repl.co/v1/api/add_products',
+    get_products_uri: 'https://e-commerce-api.aremzy.repl.co/v1/api/get_products',
+    upload_locked_product_uri: 'https://e-commerce-api.aremzy.repl.co/v1/api/lock_inventory',
+    upload_locked_product_uri_guest: 'https://e-commerce-api.aremzy.repl.co/v1/api/lock_inventory_guest',
+    fetch_orders_uri: 'https://e-commerce-api.aremzy.repl.co/v1/api/fetch_orders',
+    get_categories: 'https://e-commerce-api.aremzy.repl.co/v1/api/get_category',
+    send_otp: 'https://e-commerce-api.aremzy.repl.co/v1/api/send_otp',
+    verify_otp: 'https://e-commerce-api.aremzy.repl.co/v1/api/verify_otp',
+    change_password: 'https://e-commerce-api.aremzy.repl.co/v1/api/change_password',
+    search_uri: 'https://e-commerce-api.aremzy.repl.co/v1/api/search/search_products'
 }
 
 export default pic
