2a729746d46daf402c32fe4f908a8af1ba66d149
Aremu mahmud <74737123+aremumahmud@users.noreply.github.com>
2023-06-23 15:08:15 +0100

    Update discount.jsx

--- src/components/discount.jsx
+++ src/components/discount.jsx
@@ -21,7 +21,7 @@ function Discounts({setPage}){
               return window.open('/users/login','_self')
             }
             
-            if(!dt.sucess) return alert('Sorry an unexpected error occured!'+JSON.stringify(dt))
+            if(!dt.sucess) return alert('Sorry an unexpected error occured!'+JSON.stringify(dt._doc))
             setValueReturned(dt.value)
         })
     }
