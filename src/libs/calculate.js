341addeaf265c18237d18cd313450334e107ceb4
Aremu mahmud <74737123+aremumahmud@users.noreply.github.com>
2023-06-27 13:20:11 +0100

    Update calculate.js

--- src/libs/calculate.js
+++ src/libs/calculate.js
@@ -1,7 +1,7 @@
-import { symbolTab } from "../config/currency"
-import currencyTab from "../config/currency"
+//import { symbolTab } from "../config/currency"
+//import currencyTab from "../config/currency"
 
-function calculate(arr, symbol) {
+function calculate(arr, symbol,currencyTab,symbolTab) {
     let total = 0
     arr.forEach(d => {
         //console.log(d.price, 'd')
