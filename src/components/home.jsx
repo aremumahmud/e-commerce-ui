9d65210331b7bd1d73bd726debb958a6abf9363d
mahmud aremu <aremumahmud2003@gmail.com>
2023-06-30 11:28:44 +0100

    added some changes

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -23,7 +23,7 @@ function Home({setCart , setPage, cart, setProduct,data , symbol,setFilter ,setL
       <br /><br /> <br /><br /><br />      <Filter search={search} categories={categories} setLoad={setLoad} setFilter={setFilter} /> 
       {/* <p className="topic">Top Fashion For You!</p> */}
       <Products currencyTab={currencyTab} symbolTab={symbolTab}  setViewStatus3={setViewStatus3} setCartData3={setCartData3} symbol={symbol} setProduct={setProduct} cart={cart} setPage={setPage} setCart={setCart} data={data} />
-      {/* <Pagination /> */}
+      <Pagination />
       {/* <p className="topic">Weekly Popular Products</p>
       <Products  setViewStatus3={setViewStatus3} setCartData3={setCartData3} symbol={symbol} setProduct={setProduct}  cart={cart} setPage={setPage} setCart={setCart} data={[0, 1, 2, 3].map(i=>data[i])} />
       <br /> */}
