6f78ac89b4d5097c9c868439756509889a807414
mahmud aremu <aremumahmud2003@gmail.com>
2023-07-01 13:38:54 +0100

    added some corrections

---
+++ src/components/policy.jsx
@@ -0,0 +1,35 @@
+function Policy(){
+    return (
+        <div class="conta">
+        <div class="logo">
+            <a href='https://e-commerce-ui-ruddy.vercel.app/home' class="logo">
+                <img src="https://res.cloudinary.com/dvauarkh6/image/upload/v1686347400/DEV/vnfp8ucivmcfyyr8uipo.jpg" alt="" />
+            </a>
+        </div>
+        <div class="topic_header">
+            <div class="iconify">
+                ico
+            </div>
+            <div class="title">
+                Glitzabelle Label Return Policies
+            </div>
+        </div>
+        <div class="policies">
+            <div class="policy">
+                <p class="header">DOMESTIC SHIPPING</p>
+                <p class="description">
+                    Deliveries within Lagos take 1-3 working days while deliveries outside Lagos but within Nigeria take 4-6 working days.
+                </p>
+            </div>
+            <div class="policy">
+                <p class="header">INTERNATIONAL SHIPPING</p>
+                <p class="description">
+                    All international orders are trackable. Delivery time for international orders is 5-10 working days after order placement.
+                </p>
+            </div>
+
+        </div>
+
+    </div>
+    )
+}
