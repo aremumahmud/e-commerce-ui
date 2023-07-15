92ccd05f931895ce1cfd02a1f959f90b39db0eb0
Aremu mahmud <74737123+aremumahmud@users.noreply.github.com>
2023-07-15 14:58:51 +0100

    Update validate_deets.js

--- src/libs/validate_deets.js
+++ src/libs/validate_deets.js
@@ -1,13 +1,13 @@
 let getDoc = (x) => document.getElementsByName(x)[0]
 
 
-let validate = (...ids) => {
+let validate = (ids,isNaij) => {
     let results = []
-    for (let id = 0; id < ids[0].length; id++) {
+    for (let id = 0; id < ids.length; id++) {
         // console.log(ids)
-        let idn = ids[0][id]
+        let idn = ids[id]
             //   console.log(idn)
-        if (!getDoc(idn).value) {
+        if (!getDoc(idn).value || (isNaij&&idn==='state')) {
             //now check and convert those numbers to the id of the actual boxes
             results.push(1)
             getDoc(idn).style.border = '2px solid red'
